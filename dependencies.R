1c9883afedcf9aceed3fc8b8516528b936ef47cd
Choens <andy.choens@gmail.com>
2024-03-01 07:52:45 -0500

    Big set of changes for this year

--- dependencies.R
+++ dependencies.R
@@ -1,13 +1,17 @@
 ## Simple script used to install all packages needed by the lecture/lab.
 
 p <- c(
+  "httpgd",
   "knitr",
   "janitor",
   "markdown",
   "rio",
   "rmarkdown",
   "shiny",
+  "tidymodels",
   "tidyverse",
   "Stat2Data"
 )
 install.packages(p)
+
+unlink("quiz-answers.Rmd")
